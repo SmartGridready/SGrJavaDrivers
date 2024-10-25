2e905a2158118fbbc9d98940c6b10f4d4b6f04e5
Matthias Krebs <matthias.krebs@ateleris.ch>
2024-10-25 22:11:21 +0200

    ported HiveMQ client factory over from CommHandler code base. this one may need better decoupling from the CommHandler framework.

---
+++ HiveMq/src/main/java/com/smartgridready/driver/hivemq/HiveMqtt5MessagingClientFactory.java
@@ -0,0 +1,28 @@
+package com.smartgridready.driver.hivemq;
+
+import com.smartgridready.driver.api.messaging.GenMessagingClient;
+import com.smartgridready.driver.api.messaging.GenMessagingClientFactory;
+import com.smartgridready.driver.api.messaging.model.MessagingInterfaceDescription;
+import com.smartgridready.driver.api.messaging.model.MessagingPlatformType;
+
+public class HiveMqtt5MessagingClientFactory implements GenMessagingClientFactory {
+
+    public GenMessagingClient create(MessagingInterfaceDescription interfaceDescription) {
+
+        if (interfaceDescription.getMessagingPlatformType() == null) {
+            throw new IllegalArgumentException("No messaging platform defined in EI-XML");
+        }
+
+        if (interfaceDescription.getMessageBrokerList() == null ) {
+            throw new IllegalArgumentException("No message broker defined in EI-XML");
+        }
+
+        if (interfaceDescription.getMessagingPlatformType() != MessagingPlatformType.MQTT5) {
+            throw new IllegalArgumentException(
+                    "Wrong client factory provided (supporting MQTT5). " +
+                            "Please use a messaging client factory that supports " +
+                            interfaceDescription.getMessagingPlatformType().name());
+        }
+        return new HiveMqtt5MessagingClient(interfaceDescription);
+    }
+}
